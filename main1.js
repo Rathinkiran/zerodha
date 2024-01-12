64edb2cd6281c12dea2d3ed483c6ab9c4bab7fb9
Rathinkiran <139418953+Rathinkiran@users.noreply.github.com>
2024-01-12 20:06:36 +0530

    Added a zerodha file
    Added a zerodha file

---
+++ main1.js
@@ -0,0 +1,13 @@
+//objects,and arrays of objects
+const allusers=[{
+    name: "rathin",
+    age: 56
+},{
+    name:"mohan",
+    age:78
+},{
+    name:"harsha",
+    age:89,
+    committed:"yes"
+}]
+console.log(allusers[0]["age"])
